d1681027ec98fdece60bec2df8fa7f6ca4fec402
daloopy <dvaquera@purdue.edu>
2023-01-28 19:08:47 -0500

    Receive and repeat user input

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    println!("Enter input:");
+
+    match io::stdin().read_line(&mut input){
+        Ok(_) => {
+            println!("You said: {}", input);
+        },
+        Err(e) => println!("Something went wrong: {}", e)
+
+    };
+
 }
