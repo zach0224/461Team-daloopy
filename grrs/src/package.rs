23f9674ee80f9e80987d8f1885ccac396fea9346
Chombler <64819823+Chombler@users.noreply.github.com>
2023-02-10 17:20:24 -0500

    Implemented Regex and Reqwest
    Both Regex and Reqwest now work as expected. I use Regex to identify whether or not the url passed in is a valid github url, and if not, isolate the package name in order to poll the npm API. Reqwest is used to poll the API.

--- grrs/src/package.rs
+++ grrs/src/package.rs
@@ -1,4 +1,7 @@
 use std::fmt;
+use regex::Regex;
+use lazy_static::lazy_static;
+use reqwest;
 
 pub struct Package {
     pub total_score: i32,
@@ -32,12 +35,32 @@ pub struct URL {
     pub git_repo_url: String
 }
 
+
 impl URL {
 
     pub fn new(url: String) -> URL{
-        URL {url: url.clone(), git_repo_url: url.clone()}
+        let git_url = URL::get_git_url(&url);
+        URL {url: url.clone(), git_repo_url: git_url}
     }
 
+    fn get_git_url(url: &String) -> String{
+        lazy_static! {
+            static ref GIT_RE:Regex = Regex::new(r#"https://github\.com/.+"#).unwrap();
+            static ref NPM_RE:Regex = Regex::new(r#"https://www\.npmjs\.com/package/(.+)"#).unwrap();
+        }
+        if GIT_RE.is_match(url) {
+            println!("{} is a github URL!", url);
+            url.clone()
+        } else {
+            let cap = NPM_RE.captures(url).unwrap();
+            println!("Package Name: {:?}", &cap[1]);
+            let npm_url = format!("https://registry.npmjs.org/{}", &cap[1]);
+            println!("NPM URL: {}", npm_url);
+            let body = reqwest::blocking::get(npm_url);
+            println!("body = {:?}", body);
+            url.clone()
+        }
+    }
     pub fn get_url_string(&self) -> String{
         println!("URL from URL: {}", self);
         self.url.clone()
