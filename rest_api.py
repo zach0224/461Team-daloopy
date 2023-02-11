2419383d9dc5141b3e5e051730cc081307eddd8f
Anonya Mitra <mitra28@ececomp1.ecn.purdue.edu>
2023-02-11 01:10:38 -0500

    REST API calls working, json stuff not working

--- rest_api.py
+++ rest_api.py
@@ -1,28 +1,32 @@
 import requests
+import json
 import os
 
 
 # parsing URL file (with *only* GitHub links)
-with open(" .txt", "r") as file:
-   urls = file.readlines()
+# with open(" .txt", "r") as file:
+#    urls = file.readlines()
 
 
-my_var = os.getenv("GITHUB_TOKEN")
-if my_var:
-    # Use the value of MY_VAR
-    print("MY_VAR is set to:", my_var)
-else:
-    # MY_VAR is not set
-    print("MY_VAR is not set")
+token = os.getenv("GITHUB_TOKEN")
 
+# for url in urls:
 
-for url in urls:
+#     #get our content
+#     response = requests.get(url, auth=('daloopy', my_var))
 
-    #get our content
-    response = requests.get(url, auth=('daloopy', my_var))
+#     repository_dict = response.json()
 
-    repository_dict = response.json()
+#     #write to txt file -> go to rust for our classes and calculations
+#     with open("data.txt", "w") as file:
+#        file.writelines(repository_dict)
 
-    #write to txt file -> go to rust for our classes and calculations
-    with open("data.txt", "w") as file:
-       file.writelines(repository_dict)
+url = "https://github.com/cloudinary/cloudinary_npm"
+
+my_headers = {'Authorization' : 'Bearer {token}'}
+response = requests.get(url, headers=my_headers)
+
+# response = requests.get(url, auth=('daloopy', my_var))
+# repository_dict = response.json
+
+print(response.json)
