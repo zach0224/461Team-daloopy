a4d484df9a6da0d64d195e8bb3dee04a5d6e227a
WahabAkanbi <wakanbi@purdue.edu>
2023-02-12 14:33:56 -0500

    basic test script in main

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -65,12 +65,11 @@ fn internal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn internal() {
-        assert_eq!(4, internal_adder(2, 2));
-    }
+#[test]
+fn internal() {
+    assert_eq!(4, internal_adder(2, 2));
+}
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, add_two(2));
 }
--- grrs/src/tests/testcase.rs
+++
@@ -1,6 +0,0 @@
-use grrs;
-
-#[test]
-fn it_adds_two() {
-    assert_eq!(4, adder::add_two(2));
-}
